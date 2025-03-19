beab2cfcef80ba48c10af935f7c1dac1acea2485
Deepak Yadav <deepakyadav11767@gmail.com>
2025-03-19 10:03:52 +0000

    datatypes

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -32,3 +32,30 @@ const myFunction = function(){
 }
 
 console.log(typeof obj)
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory is of two types 
+// Stack(Primitive) , Heap(Non Primitive)
+// Stack 
+let myYoutubeName = "DeepakCodes"
+let anotherName = myYoutubeName
+anotherName ="ChaiAurCode"
+
+console.log(myYoutubeName)
+console.log(anotherName)
+// anotherName will get the copy of the myYoutubeName so when 
+// we change its value the orignal value will not change
+
+// Heap 
+let userOne = {
+    email:  "useR@gmail.com",
+    upi: "abs@payu"
+}
+let userTwo = userOne
+
+// userTwo will get the reference of the same userOne value
+userTwo.email = "abcd@gmail.com" //this will change the value of email of the original value
+
+console.log(userOne)
+console.log(userTwo)
