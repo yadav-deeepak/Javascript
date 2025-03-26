c256ade395f4708466a822fb377aec2e7ffeb129
Deepak Yadav <deepakyadav11767@gmail.com>
2025-03-26 17:13:28 +0000

    MathAndNum

--- 01_basics/06_numsAndMath.js
+++ 01_basics/06_numsAndMath.js
@@ -18,6 +18,16 @@ console.log(hundreds.toLocaleString('en-IN')) // 10,00,000
 // MAX_VALUE AND MIN_VALUE will tell the maximum and minimum value of the number
 
 // ++++++++++++++++++ Maths +++++++++++++++++++++++
-console.log(Math) // returns an object Math
-console.log(Math.abs(-4)) // converts number into positive
-console.log(Math.round(4.6)) // round off the value
+// console.log(Math) // returns an object Math
+// console.log(Math.abs(-4)) // converts number into positive
+// console.log(Math.round(4.6)) // round off the value
+// console.log(Math.ceil(4.2)) //5
+// console.log(Math.floor(4.2)) //4
+// console.log(Math.min(2,4,5,6))// returns the minimum value
+
+console.log(Math.floor(Math.random()*10) + 1)// generates random value between 0 and 1
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min ))
